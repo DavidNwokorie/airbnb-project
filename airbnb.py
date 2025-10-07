b4b8e02451a57034bdf4677fc27c47bac866e849
David Nwokorie <dnwokori@ttu.edu>
2025-10-07 16:24:44 -0500

    added some more missing values

--- airbnb.py
+++ airbnb.py
@@ -93,6 +93,5 @@ airbnb['minimum_nights_missing'] = airbnb['minimum_nights'].isna().astype(int)
 airbnb['minimum_nights'] = airbnb['minimum_nights'].fillna(1)
 
 
-# should be all good now i hope lol
 
 
