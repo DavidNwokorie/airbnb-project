e8370db73bd797a3d4723ccf6d82b20007e27803
David Nwokorie <dnwokori@ttu.edu>
2025-10-07 20:36:27 -0500

    finished up with the missing values

--- airbnb.py
+++ airbnb.py
@@ -101,12 +101,38 @@ airbnb['number_of_reviews_missing'] = airbnb['number_of_reviews'].isna().astype(
 airbnb['number_of_reviews'] = airbnb['number_of_reviews'].fillna(0)
 
 
-airbnb.drop("last_review_time")
+# filling missing data for last review time
+# exact dates not needed so turned into a binary dummy, meaning if it has a date it was reviewed, if not it wasn't reviewed
+# likewise dropped the original column after because it is not needed
 airbnb['last_review_misisng'] = airbnb['last_review_time'].isna().astype(int)
+airbnb = airbnb.drop(columns=['last_review_time'])
 
 
+# filling missing data for reviews per month 
+# created a binary dummy holding if the property has been reviewed or not
+# if it has not been reviewed it'll be 0, if it has it'll be 1
 airbnb['reviews_per_month_missing'] = airbnb['reviews_per_month'].isna().astype(int)
 airbnb['reviews_per_month'] = airbnb['reviews_per_month'].fillna(0)
 
 
+# filling missing data for star rating
+# created a binary dummy as well to just inticate no guest rating yet
+# including the 0 doesn't mean bad quality just "no rating yet"
+airbnb['star_rating_missing'] = airbnb['star_rating'].isna().astype(int)
+airbnb['star_rating'] = airbnb['star_rating'].fillna(0)
 
+
+# filling missing data for host listing count
+# made binary to keep it notiable where it was missing
+# filled missing value with 1 because host needs at least 1 property to use airbnb
+airbnb['host_listings_count_missing'] = airbnb['host_listings_count'].isna().astype(int)
+airbnb['host_listings_count'] = airbnb['host_listings_count'].fillna(1)
+
+
+# filling missing data for available days in the future
+# saw some negative numbers which doesn't make sense, so i changed them to zero
+# also changed all missing values to equal 0
+airbnb.loc[airbnb['available_days_in_future'] < 0, 'available_days_in_future'] = 0
+airbnb['available_days_missing'] = airbnb['available_days_in_future'].isna().astype(int)
+airbnb['available_days_in_future'] = airbnb['available_days_in_future'].fillna(0)
+print(airbnb['available_days_in_future'].describe())
